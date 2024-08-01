6d1f453e26831d8449f74ff8b813cffc1c5db4e1
New-Tong <1248449622@qq.com>
2024-08-01 20:17:26 +0800

    开发数据库持久层对象通用属性

---
+++ admin/src/main/java/com/newtong/shortlink/admin/common/database/BaseDO.java
@@ -0,0 +1,34 @@
+package com.newtong.shortlink.admin.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author NewTong
+ * @Date 2024/8/1 -20:10
+ * @Description
+ */
+public class BaseDO implements Serializable{
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT, value = "create_time")
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE, value = "update_time")
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT, value = "del_flag")
+    private int delFlag;
+}
