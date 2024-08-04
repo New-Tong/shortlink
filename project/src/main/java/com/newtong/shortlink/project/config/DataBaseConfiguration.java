2047d8edcf7dad5af51fa4f2157f1a05fc930ef7
New-Tong <1248449622@qq.com>
2024-08-04 12:08:34 +0800

    feature：新增MySql数据库分页插件配置

---
+++ project/src/main/java/com/newtong/shortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,22 @@
+package com.newtong.shortlink.project.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author NewTong
+ * @Date 2024/8/4 -11:54
+ * @Description
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
